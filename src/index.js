cb625484c520328ef244639d3d7e48cc2d389092
fuzhiquan <fuzhiquan@beisen.com>
2020-03-16 22:49:02 +0800

    feat: 监控数据通过new Image().src发送给后端

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@
  * 5. 用户行为
  */
 import preformance from './preformance'
-
+function formatObj(obj) {
+    const result = []
+    for(let key in obj) {
+        result.push(`${key}=${obj[key]}`)
+    }
+    return result.join('&')
+}
+/**
+ * 把监控的数据通过new Image().src这种方式发送给后端，下面这段代码的方式是为了防止浏览器垃圾回收
+ */
 preformance.init((data) => {
-    console.log(data)
+    var n = "log_"+ (newDate()).getTime()
+    var c = window[n] = new Image()//把new Image()赋给一个全局变量长期持有
+    c.onload = (c.οnerrοr = function(){window[n] = null})
+    c.src = `/p.gif?${formatObj(data)}`
+    c = null//释放局部变量c
 })
